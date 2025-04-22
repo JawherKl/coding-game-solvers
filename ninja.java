c6d8ebe16529f56b39cfcb235fc8b4736c06daba
Jawher Kl <kalleljawher4@gmail.com>
2025-04-22 23:33:42 +0100

    Create ninja.java

---
+++ ninja.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int MMM = in.nextInt();
+        int AAA = in.nextInt();
+        int NNN = in.nextInt();
+
+        // Total Cost
+        int totalCost = MMM * NNN * ( NNN + 1 ) / 2;
+        int borrow = totalCost - AAA;
+
+        System.out.println(Math.max(borrow, 0));
+    }
+}
